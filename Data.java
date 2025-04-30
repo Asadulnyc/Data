3159080a70872dcac1ec080f6de8354cc062cef2
Asadulnyc <asadulk@nycstudents.net>
2025-04-30 15:37:01 +0000

    ich bin fertig

--- Data.java
+++ Data.java
@@ -3,14 +3,38 @@ public class Data {
     private int[][] grid;
 
     public Data(int r, int c) {
-        int[][] grid = new int[r][c];
+        grid = new int[r][c];
     }
 
     public void repopulate() {
-        
+        for (int r = 0; r<grid.length; r++) {
+            for (int c = 0; c<grid[r].length; c++) {
+                do grid[r][c] = (int) ((Math.random() * MAX)+1)/10*10;
+                while (grid[r][c]%100 == 0);
+            }
+        }
     }
 
     public int countIncreasingCols() {
+        int count = 0;
+        for (int c = 0; c<grid[0].length;c++) {
+            boolean isIncreasing = true;
+            for (int r = 0; r<grid.length-1; r++) {
+                if (grid[r][c]> grid[r+1][c]) isIncreasing=false;
+            }
+            if (isIncreasing) count++;
+        }
+        return count;
+    }
 
+    public String toString() {
+        String s = "";
+        for (int[] arr : grid) {
+            for (int a : arr) {
+                s+= a + ", ";
+            }
+            s+= "\n";
+        }
+        return s;
     }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println(Data.MAX);
+        Data a = new Data(3,3);
+        a.repopulate();
+        System.out.println(a);
+        System.out.println(a.countIncreasingCols());
+
+        Data b = new Data(4,2);
+        b.repopulate();
+        System.out.println(b);
+        System.out.println(b.countIncreasingCols());
     }
 }
